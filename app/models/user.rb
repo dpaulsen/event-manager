db06445d29deeaefb75f576dca87dfbc7796ffbb
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-11-02 19:31:12 -0500

    Added user role levels and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
 
+  enum role: {deactivated: 0, member: 1, organizer: 2, admin: 3, superuser: 9999}
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :member
+  end
+
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,69 @@ RSpec.describe User, type: :model do
 
   it {should have_valid(:email).when("user@fake.com")}
 
-  pending "add some user roles"
+  describe "#deactivated?" do
+    it "is not an deactivated if the role is not deactivated" do
+      user = FactoryBot.create(:user, role: :member)
+      expect(user.deactivated?).to eq(false)
+    end
 
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    it "is an deactivated if the role is deactivated" do
+      user = FactoryBot.create(:user, role: :deactivated)
+      expect(user.deactivated?).to eq(true)
+    end
+  end
+
+  describe "#member?" do
+    it "is not a member if the role is not memeber" do
+      user = FactoryBot.create(:user, role: :deactivated)
+      expect(user.member?).to eq(false)
+    end
+
+
+    it "is a member if the role is memeber" do
+      user = FactoryBot.create(:user, role: :member)
+      expect(user.member?).to eq(true)
+    end
+  end
+
+  describe "#organizer?" do
+    it "is not an organizer if the role is not organizer" do
+      user = FactoryBot.create(:user, role: :member)
+      expect(user.organizer?).to eq(false)
+    end
+
+
+    it "is an organizer if the role is organizer" do
+      user = FactoryBot.create(:user, role: :organizer)
+      expect(user.organizer?).to eq(true)
+    end
+  end
+
+  describe "#admin?" do
+    it "is not an admin if the role is not admin" do
+      user = FactoryBot.create(:user, role: :member)
+      expect(user.admin?).to eq(false)
+    end
+
+    it "is an admin if the role is admin" do
+      user = FactoryBot.create(:user, role: :admin)
+      expect(user.admin?).to eq(true)
+    end
+  end
+
+  describe "#superuser?" do
+    it "is not a superuser if the role is not superuser" do
+      user = FactoryBot.create(:user, role: :member)
+      expect(user.superuser?).to eq(false)
+    end
+
+    it "is a superuser if the role is superuser" do
+      user = FactoryBot.create(:user, role: :superuser)
+      expect(user.superuser?).to eq(true)
+    end
+  end
+
+  pending "Update models to do functions for multiple levels? "
+
 end
