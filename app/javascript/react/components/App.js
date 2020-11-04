e633a482f5896ee05cbc1b385535d5f5d22b9a16
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-11-04 16:37:28 -0500

    Adding testlayout which has a sketch of styling for main page

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {BrowserRouter,Route} from 'react-router-dom'
 import PageLayout from "./PageLayout"
+import TestLayout from "./TestLayout"
 
 export const App = (props) => {
   return (<div>
-    <h1>Make It So React</h1>
     <BrowserRouter>
-      <Route path ="/" component = {PageLayout}/>
+      <Route path ="/" component = {TestLayout}/>
      </BrowserRouter>
 
   </div>)
