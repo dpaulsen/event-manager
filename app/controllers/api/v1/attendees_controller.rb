785f6d96070e45476f76acb6e0c4603b50fbe78e
Daniel Paulsen <danielpaulsen@Daniels-MBP.home>
2020-11-07 17:27:43 -0500

    Added button mode change functionality, change state working.

--- app/controllers/api/v1/attendees_controller.rb
+++ app/controllers/api/v1/attendees_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::AttendeesController < ApiController
 
   def index
-    binding.pry
+    #binding.pry
     render json: Attendee.all
   end
 
   def show
-    binding.pry
+    #binding.pry
     attendee = Attendee.find(params[:id])
     render json: attendee  #, serializer: AttendeeSerializer  #notworking TODO
   end
--- app/javascript/react/components/attendee/AttendeeSingleContainer.js
+++ app/javascript/react/components/attendee/AttendeeSingleContainer.js
@@ -5,9 +5,15 @@ import AttendeeShowPage from "./AttendeeShowPage"
 
 const AttendeeSingleContainer =(props) =>{
 
+  const modes = { 
+    SHOW: 'show',
+    EDIT: 'edit',
+    DELETE: 'delete'
+  }
+
   let id = props.match.params.id
 
-  
+  const [mode, setMode] = useState(modes.SHOW)
 
   const [attendee, setAttendee] = useState({
     id: null,
@@ -47,7 +53,69 @@ const AttendeeSingleContainer =(props) =>{
     debugger
     displayTile = (<AttendeeShowPage attendee = {attendee}
     />)
+  } else{ 
+  displayTile = (<p> that is not implemented</p>)
+}
+  
+  let modeTemp = null;
+  switch (mode) {
+    case modes.SHOW:
+      modeTemp =(<p> mode is show </p>);
+      break;
+    case modes.EDIT:
+      modeTemp =(<p> mode is edit </p>);
+      break;
+    case modes.DELETE:
+      modeTemp =(<p> mode is delete </p>);
+      break;
+    default: 
+      modeTemp =(<p> error should never get here </p>);
   }
+
+  const onModeClick = (event) =>{
+    debugger
+    switch (event.currentTarget.id) {
+      case "edit-attendee":
+        setMode(modes.EDIT);
+        break;
+      case "delete-attendee":
+        setMode(modes.DELETE);
+        break;
+      case "show-attendee":
+        setMode(modes.SHOW);
+        break;
+      default :
+        console.error(`that mode ${event.currentTarget.id} is not recognized`)
+    }
+  }
+  
+  let buttonModesGroup = null;
+
+  buttonModesGroup = (<div>
+    <button 
+    type = "button"
+    id = "edit-attendee"
+    onClick = {onModeClick}
+    >
+    Edit
+    </button>
+    <button 
+    type = "button"
+    id = "delete-attendee"
+    onClick = {onModeClick}
+    >
+    Delete
+    </button>
+    <button 
+    type = "button"
+    id = "show-attendee"
+    onClick = {onModeClick}
+    >
+    Show
+    </button>
+  </div>);
+
+ 
 
   return( 
     <div>
@@ -57,6 +125,9 @@ const AttendeeSingleContainer =(props) =>{
         </div>
         <div>
           <p> button choices </p> 
+          {buttonModesGroup}
+          <hr/>
+          {modeTemp}
         </div>
       </div>
 
